73dc30e64bddf735f71619480442a9afcdbc7396
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-10-04 18:45:41 -0400

    ChatGPT-Bot

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import Chat from '../components/Chat'
+import Providers from '../components/Providers'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'BookBuddy',
+  description: 'Your bookstore for fantasy & mystery novels',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+   <Providers>
+          <body className={inter.className}>
+           <Chat />
+          {children}
+          </body>
+   </Providers>
+     
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center inset-0 absolute">
+        Web Content
+    </main>
+  )
+}
